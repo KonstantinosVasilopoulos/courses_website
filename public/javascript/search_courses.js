0443a406ed1f79ec695c7f28165a157274fb0d40
KonstantinosVasilopoulos <costasvas34@protonmail.com>
2022-01-10 20:02:45 +0200

    trying to display search results

--- public/javascript/search_courses.js
+++ public/javascript/search_courses.js
@@ -1,20 +1,25 @@
 const API_URL = 'https://elearning-aueb.herokuapp.com/'
 
 /* Query and fetch API data using a provided keyword */
-function searchCourse() {
+const searchBtn = document.getElementById('search-keyword-btn')
+searchBtn.addEventListener('click', (event) => {
+    event.preventDefault()
+
     // Retrieve the keyword from it's input field
     const keyword = document.getElementById('courseName').value
 
     // Fetch the courses by title
-    fetch(API_URL + 'courses/search?title=' + keyword, {
-        mode: 'cors',
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-        }
-    })
+    fetch(API_URL + 'courses/search?title=' + keyword)
     .then(response => response.json())
     .then(data => {
-        console.log(data)
+        // Get and compile the handlebars template
+        const source = document.getElementById('course-results-preview').innerHTML
+        const template = Handlebars.compile(source)
+
+        // Create a new preview for each course fetched
+        data.forEach((course) => {
+            console.log(course)
+        })
     })
-    .catch(error => console.log(error))
-}
+    .catch(err => console.log(err))
+})
