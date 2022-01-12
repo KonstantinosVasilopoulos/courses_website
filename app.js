c74ebc4aa48eb41a10ae55ea4b177826d96774d1
GeoMil <geo_mil@windowslive.com>
2022-01-12 14:15:18 +0200

    styling search results and trying to obtain images' url

--- app.js
+++ app.js
@@ -49,8 +49,29 @@ app.get("/register", (req, res, next) => {
 // Single course page
 app.get("/courses", (req, res, next) => {
   res.render("courses", { layout: "main" });
-  console.log("Served courses.")
+  console.log("Served courses.");
 });
 
 // Make the app listen to port 3000
 app.listen(port, () => console.log(`App listening to port ${port}.`));
+
+// register new functions
+/* handlebars.registerHelper("link", function (url) {
+  var url = Handlebars.escapeExpression(url);
+  return new Handlebars.SafeString("<img src='https://elearning-aueb.herokuapp.com/static/images/" + url + "'>");
+}); */
+
+/* handlebars.registerHelper("link", function (url) {
+  var url = Handlebars.escapeExpression(url);
+  return new Handlebars.SafeString("<img src='https://elearning-aueb.herokuapp.com/static/images/" + url + "'>");
+}); */
+
+var hbs = handlebars.create({
+  // Specify helpers which are only registered on this instance.
+  helpers: {
+    link: function (url) {
+      var url = Handlebars.escapeExpression(url);
+      return new Handlebars.SafeString("<img src='https://elearning-aueb.herokuapp.com/static/images/" + url + "'>");
+    },
+  },
+});
